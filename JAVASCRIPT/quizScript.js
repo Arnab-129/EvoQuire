4fb5bc44d3831722d5cc7816d308a27287fc57ea
EmberTSeal <trishaseal49@gmail.com>
2023-08-27 09:45:08 +0530

    Add quiz correct-wrong checking and explanation

--- JAVASCRIPT/quizScript.js
+++ JAVASCRIPT/quizScript.js
@@ -4,7 +4,9 @@ quizBg = document.getElementById('quizBg')
 quizQuestions = document.getElementById('quiz-questions')
 submitBtn = document.getElementById('answer-submit')
 options = Array.from(document.getElementById('answers').children)
-
+correctAnsID = 2
+selectedIndex = -1
+explainedAns = document.getElementById('explanation')
 
 attemptButton.addEventListener("click", ()=>{
     instructions.innerHTML = '';
@@ -23,6 +25,21 @@ options.forEach(option =>
                 option.classList.remove('selected');
         })
     })
-    
 )
+
+submitBtn.addEventListener('click', ()=>{
+    if(selectedIndex === -1){
+        console.log('Please select an answer first!');
+    }
+    else{
+        if(selectedIndex === correctAnsID){
+            options[selectedIndex].classList.add('correctChoice');
+        }
+        else{
+            options[selectedIndex].classList.add('wrongChoice');
+            options[correctAnsID].classList.add('correctChoice');
+        }
+        explainedAns.style.display = 'block';
+    }
+})
 
