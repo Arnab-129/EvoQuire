d83c1f04bdb553d78504a27feada46d055ea0bb7
EmberTSeal <trishaseal49@gmail.com>
2023-08-27 09:33:28 +0530

    quiz question show and select added

--- JAVASCRIPT/quizScript.js
+++ JAVASCRIPT/quizScript.js
@@ -1,9 +1,28 @@
 attemptButton = document.getElementById('attempt-button')
 instructions = document.getElementById('instruct')
 quizBg = document.getElementById('quizBg')
+quizQuestions = document.getElementById('quiz-questions')
+submitBtn = document.getElementById('answer-submit')
+options = Array.from(document.getElementById('answers').children)
+
 
 attemptButton.addEventListener("click", ()=>{
     instructions.innerHTML = '';
     quizBg.style.display = 'none';
     attemptButton.style.display = 'none';
+    quizQuestions.style.display = 'block';
 })
+
+options.forEach(option => 
+    option.addEventListener('click', ()=>{
+        option.classList.add('selected');
+        selectedIndex = options.indexOf(option) ;
+        // remove other selections when one is selected
+        options.forEach(option => {
+            if(options.indexOf(option)!==selectedIndex && option.classList.contains('selected'))
+                option.classList.remove('selected');
+        })
+    })
+    
+)
+
